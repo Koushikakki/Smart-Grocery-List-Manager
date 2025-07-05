14fe8fe47fe1d103d6cdc9138d4cdc1ab8b3a1dc
admin <admin@MacBookPro.lan>
2025-07-05 11:37:00 +0530

    Get HTML elements using their IDs

---
+++ script.js
@@ -0,0 +1,7 @@
+const form = document.getElementById('grocery-form');
+const list = document.getElementById('grocery-list');
+const totalCount = document.getElementById('total-count');
+const remainingCount = document.getElementById('remaining-count');
+
+let groceryItems = [];
+
